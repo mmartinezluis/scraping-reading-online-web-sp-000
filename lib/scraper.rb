1491882a906a64cad9d65d8983b05b15dfa40802
Luis Martinez <luis_mmartinez@live.com>
2021-01-09 00:43:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,20 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 puts doc
+<<<<<<< HEAD
+=======
+
+doc.css("element_id")
+doc.css("element_id").text
+doc.css("class1.class2")
+
+couses= doc.css(class1.class2.image1)
+
+couses.each {|cousse| puts course.text.strip}
+
+couses= doc.css(class1.class2.image1)[0]
+
+couses= doc.css(class1.class2.image1)[0].name
+
+couses= doc.css(class1.class2.image1)[0].attributes
+>>>>>>> 6d87abb7faf71f18c6f97aeab163e6978a00e423
